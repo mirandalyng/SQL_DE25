6e530719ee7b7ae78af00e2f64e34cdb63997097
Miranda Lyng <lyngmiranda@gmail.com>
2025-11-21 13:53:36 +0100

    cleaning data lecture

--- code_along_lectures/lecture_21_nov_clean_string_squaddata/sql/clean_data.sql
+++ code_along_lectures/lecture_21_nov_clean_string_squaddata/sql/clean_data.sql
@@ -52,3 +52,41 @@ FROM
     staging.squad
 WHERE
     context LIKE CONCAT (title, '%');
+
+--if you use regular expression fuction _ will be a literal character 
+SELECT
+    *
+FROM
+    staging.squad
+WHERE
+    regexp_matches (context, CONCAT ('^', title));
+
+--task 4 
+--two key values text and [] , answer start that matches with a value 
+--create a new (not to the table but  show in the result) which is the first answer from the AI model
+--without pattern matching or regular expression 
+
+SELECT 
+  answers[18:], --slicing from 18
+  answers[18], --indexing 
+  --identifying empty answers 
+  CASE 
+    WHEN answers[18] = ',' THEN NULL 
+    ELSE answers[18:] 
+    END AS striped_answers, 
+    --identifying the position of the first '
+    INSTR(striped_answers,'''') AS first_quotation_index, -- a single quotation needs to be typed as ''
+    striped_answers[:first_quotation_index -1] as first_answer,
+    answers
+FROM 
+  staging.squad; 
+
+--task 5 
+--generate the same result as task 4 but 
+--using pattern matching 
+SELECT 
+  --regexp extract 
+  regexp_extract(answers, '''([^'']+)'',') AS first_answer,
+  
+  answers
+FROM staging.squad; 
