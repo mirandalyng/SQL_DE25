c2600197c745f9321e5fb023b7131e1437e0f282
Miranda Lyng <lyngmiranda@gmail.com>
2025-11-24 14:34:43 +0100

    EX 00

---
+++ Exercises/Ex0/sql/hemnet.sql
@@ -0,0 +1,65 @@
+
+
+CREATE TABLE IF NOT EXISTS hemnet_data AS (
+    SELECT
+        *
+    FROM
+        read_csv_auto ('data/hemnet_data_clean.csv')
+);
+
+--wildcard % 
+SELECT
+    *
+FROM
+    hemnet_data
+WHERE
+    address LIKE '%40';
+
+--e) Find out the 5 most expensive homes sold.
+SELECT
+    *
+FROM
+    hemnet_data
+ORDER BY
+    final_price DESC
+LIMIT
+    5;
+
+--f) Find out the 5 cheapest homes sold.
+SELECT
+    *
+FROM
+    hemnet_data
+ORDER BY
+    final_price ASC
+LIMIT
+    5;
+
+
+-- g) Find out statistics on minimum, mean, median and maximum prices of homes sold.
+SELECT
+    MAX(final_price) AS max_price,
+    MIN(final_price) AS min_price,
+    AVG(final_price) AS avg_price,
+    MEDIAN(final_price) AS median_price
+FROM
+    hemnet_data;
+
+--i) How many unique communes are represented in the dataset.
+SELECT
+    COUNT(DISTINCT commune) AS unique_communes
+FROM
+    hemnet_data
+
+--j) How many percentage of homes cost more than 10 million?
+SELECT
+    100.0 * COUNT(*) / (
+        SELECT
+            COUNT(*)
+        FROM
+            hemnet_data
+    ) AS percent_over_10m
+FROM
+    hemnet_data
+WHERE
+    final_price > 10000000;
--- Exercises/Ex0/sql/ingest_data.sql
+++ Exercises/Ex0/sql/ingest_data.sql
@@ -4,4 +4,27 @@
 ==========================
 */
 
--- 
+-- CREATE TABLE 
+CREATE TABLE IF NOT EXISTS course_structure (
+    content VARCHAR, 
+    week INTEGER, 
+    content_type VARCHAR 
+); 
+
+--ingest data 
+INSERT INTO
+     course_structure (content, week, content_type)
+VALUES
+('00_intro', 46, 'lecture'),
+    ('01_course_structure', 46, 'lecture'),
+    ('02_setup_duckdb', 46, 'lecture'),
+    ('exercise_0', 46, 'exercise'),
+    ('tenta', 51, 'exam'); 
+
+--select all lecutres 
+SELECT
+    *
+FROM
+    course_structure
+WHERE
+    content_type = 'lecture';
