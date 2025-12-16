3357817fa420da0655b0d01219b5b8c2c85d0ac2
Miranda Lyng <lyngmiranda@gmail.com>
2025-12-16 11:31:19 +0100

    Crash course lecture 13-15

---
+++ code_along_lectures/16_dec_Extra_lecture/utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import duckdb
+
+
+def query_database(query: str, db_path: str = "data/lecture_13_15.duckdb") -> pd.DataFrame:
+    """convenience function to open a duckdb database connection and query it"""
+    with duckdb.connect(db_path) as conn:
+        return conn.execute(query=query).df()
